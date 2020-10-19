58827581caa37b76d563ee1bcbde8c8bd54e92d7
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-10-19 16:07:37 +1300

    Added S3 read & EventBridget publish policies to the execution role

--- iam.tf
+++ iam.tf
@@ -13,6 +13,23 @@ data "aws_iam_policy_document" "lambda_application_assume_role_statement" {
   }
 }
 
+data "aws_iam_policy_document" "event_bridge_internal_entrypoint" {
+  statement {
+    sid = "LambdaApplicationInternalEntrypointEventBridgeActions"
+
+    effect = "Allow"
+
+    actions = [
+      "events:putEvents"
+    ]
+
+    resources = [
+      "*"
+    ]
+
+  }
+}
+
 resource "aws_iam_role" "lambda_application_execution_role" {
   name = format("ExecutionRole-Lambda-%s", var.application_name)
 
@@ -21,9 +38,25 @@ resource "aws_iam_role" "lambda_application_execution_role" {
   tags = merge({ Name = format("%s-Execution-Role", var.application_name) }, { "Lambda Application" = var.application_name }, var.tags)
 }
 
+resource "aws_iam_policy" "event_bridge_internal_entrypoint" {
+  name        = "LambdaApplication-${replace(var.application_name, "/-| |_/", "")}-EventBridgeInternalEntrypointPolicy"
+  policy      = data.aws_iam_policy_document.event_bridge_internal_entrypoint.json
+  description = "Grants permissions to write inernal entrypoint events to EventBridge"
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_application_logs" {
   role       = aws_iam_role.lambda_application_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_default_read_access" {
+  role       = aws_iam_role.lambda_application_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "event_bridge_internal_entrypoint_access" {
+  role       = aws_iam_role.lambda_application_execution_role.name
+  policy_arn = aws_iam_policy.event_bridge_internal_entrypoint.arn
 }
 
 resource "aws_iam_role_policy_attachment" "datastore_s3_access_policy" {
