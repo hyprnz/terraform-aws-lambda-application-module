7e06eab40bea0df51d5ca147f5f056ea4670119d
Liqiu Huang <akinoniku@gmail.com>
2021-02-17 16:02:00 +1300

    add lambda service module(bug fix)

--- api-gateway.tf
+++ api-gateway.tf
@@ -9,7 +9,7 @@ resource "aws_apigatewayv2_api" "api-gateway" {
 resource "aws_lambda_permission" "allow_apigateway" {
   statement_id  = "AllowExecutionFromApiGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_application.function_name
+  function_name = aws_lambda_function.lambda_application[0].function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_number}:${aws_apigatewayv2_api.api-gateway[0].id}/*"
   count         = var.enable_api_gateway ? 1 : 0
