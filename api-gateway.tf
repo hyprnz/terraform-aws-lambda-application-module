fc322907b7e12768858671fd1811159b9a36da0c
Liqiu Huang <akinoniku@gmail.com>
2021-02-18 13:29:55 +1300

    add lambda service module(fix api)

--- api-gateway.tf
+++ api-gateway.tf
@@ -2,14 +2,14 @@ resource "aws_apigatewayv2_api" "api-gateway" {
   name          = var.api_gateway_name
   protocol_type = "HTTP"
   route_key     = "$default"
-  target        = aws_lambda_function.lambda_application[0].arn
+  target        = aws_lambda_function.lambda_application.service.arn
   count         = var.enable_api_gateway ? 1 : 0
 }
 
 resource "aws_lambda_permission" "allow_apigateway" {
   statement_id  = "AllowExecutionFromApiGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_application[0].function_name
+  function_name = aws_lambda_function.lambda_application.service.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_number}:${aws_apigatewayv2_api.api-gateway[0].id}/*"
   count         = var.enable_api_gateway ? 1 : 0
