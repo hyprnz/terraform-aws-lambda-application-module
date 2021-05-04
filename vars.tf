e17a17de7a999bd33b0ae06437efb2e3c6ce1775
Liqiu Huang <akinoniku@gmail.com>
2021-05-04 15:04:40 +1200

    add default for msk_event_source_config

--- vars.tf
+++ vars.tf
@@ -21,11 +21,13 @@ variable "internal_entrypoint_config" {
 variable "msk_arn" {
   type        = string
   description = "the MSK source arn for all lambda requires MSK"
+  default     = ""
 }
 
 variable "msk_event_source_config" {
   type        = map(any)
   description = "Map of configurations of MSK event source for each lambda"
+  default     = {}
 }
 
 variable "artifact_bucket" {
