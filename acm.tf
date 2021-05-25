72c8999c28bbd4e6eeb9ea290bed1f854d7e9666
Reuben Dunn <reuben.dunn@hypr.co.nz>
2021-05-25 12:49:29 +1200

    Fixed bug in acm.tf that would fail for a configuration that does not require an api-gateway resource

--- acm.tf
+++ acm.tf
@@ -17,7 +17,7 @@ resource "aws_acm_certificate_validation" "cert" {
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.cert[0].domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cert[*].domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
