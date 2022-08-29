4af4629684e6565ade04526d4a81bc75bbc5414e
Nathan Wang <nathanbotinker@gmail.com>
2022-08-29 17:46:01 +1200

    Make the load balancer resource optional

--- vars.tf
+++ vars.tf
@@ -159,6 +159,12 @@ variable "tags" {
   default     = {}
 }
 
+variable "enable_load_balancer" {
+  type        = bool
+  description = "Allow to create load balancer"
+  default     = false
+}
+
 variable "service_target_group_name" {
   type        = string
   description = "The service target group attached to application load balancer listener"
