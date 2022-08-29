fd642f5c39abf6e7257fb63bb17e195f851be7ec
Nathan Wang <nathanbotinker@gmail.com>
2022-08-29 17:45:34 +1200

    Make the load balancer resource optional

--- target_group.tf
+++ target_group.tf
@@ -1,29 +1,37 @@
 resource "aws_lb_target_group" "alb_lambda_target_group" {
+  #  TODO: test when enable_load_balancer is true
+  count       = var.enable_load_balancer ? 1 : 0
   name        = var.service_target_group_name
   target_type = "lambda"
 }
 
 resource "aws_lambda_permission" "alb_lambda_permission" {
+  #  TODO: test when enable_load_balancer is true
+  count         = var.enable_load_balancer ? 1 : 0
   statement_id  = "AllowLambdaExecutionFromAlb"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_alias.lambda_application_alias["${var.lambda_alb_config.function_key}"].arn
 
   principal  = "elasticloadbalancing.amazonaws.com"
-  source_arn = aws_lb_target_group.alb_lambda_target_group.arn
+  source_arn = aws_lb_target_group.alb_lambda_target_group[0].arn
 }
 
 resource "aws_lb_target_group_attachment" "alb_lambda_target_group_attachment" {
-  target_group_arn = aws_lb_target_group.alb_lambda_target_group.arn
+  #  TODO: test when enable_load_balancer is true
+  count            = var.enable_load_balancer ? 1 : 0
+  target_group_arn = aws_lb_target_group.alb_lambda_target_group[0].arn
   target_id        = aws_lambda_alias.lambda_application_alias["${var.lambda_alb_config.function_key}"].arn
   depends_on       = [aws_lambda_permission.alb_lambda_permission]
 }
 
 resource "aws_lb_listener_rule" "alb_lambda_listener_rule" {
+  #  TODO: test when enable_load_balancer is true
+  count        = var.enable_load_balancer ? 1 : 0
   listener_arn = var.alb_lambda_listener_arn
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_lambda_target_group.arn
+    target_group_arn = aws_lb_target_group.alb_lambda_target_group[0].arn
   }
 
   condition {
