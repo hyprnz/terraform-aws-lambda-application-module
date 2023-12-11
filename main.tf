3267e1be9277e3fe5691a0fa48eeefad18e0397d
Reuben Dunn <reuben.dunn@hypr.co.nz>
2023-12-11 15:50:40 +1300

    Fixed missing qualifier property for the aws_lambda_permission resource. Closes #24

--- main.tf
+++ main.tf
@@ -77,9 +77,10 @@ resource "aws_lambda_permission" "internal_entrypoints" {
 
   statement_id  = replace(title(each.value.name), "/-| |_/", "")
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda_application[each.key].function_name}:${var.alias_name}"
+  function_name = aws_lambda_function.lambda_application[each.key].function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.internal_entrypoint[each.key].arn
+  qualifier     = aws_lambda_alias.lambda_application_alias[each.key].name
 }
 
 # resource "aws_lambda_permission" "external_entrypoints" {
