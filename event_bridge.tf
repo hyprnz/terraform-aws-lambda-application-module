984005360647c231cf7138ea14997a5d7fd4fcbd
Bing Shi <bing@hypr.nz>
2024-02-14 13:33:39 +1300

    Add optional event bus name and lifecycle management for CloudWatch events

--- event_bridge.tf
+++ event_bridge.tf
@@ -6,6 +6,7 @@ resource "aws_cloudwatch_event_rule" "internal_entrypoint" {
 
   event_pattern       = contains(keys(each.value), "event_pattern_json") ? jsonencode(each.value.event_pattern_json) : null
   schedule_expression = contains(keys(each.value), "schedule_expression") ? each.value.schedule_expression : null
+  event_bus_name      = try(each.value.event_bus_name, "default")
 
   tags = merge({ Name = format("%s-%s", var.application_name, each.value.name) }, { "Lambda Application" = var.application_name }, { "version" = var.application_version }, var.tags)
 }
@@ -15,4 +16,11 @@ resource "aws_cloudwatch_event_target" "lambda_internal_entrypoint" {
   rule      = aws_cloudwatch_event_rule.internal_entrypoint[each.key].name
   target_id = each.value.name
   arn       = aws_lambda_alias.lambda_application_alias[each.key].arn
+
+  event_bus_name = try(each.value.event_bus_name, "default")
+  lifecycle {
+    replace_triggered_by = [
+      aws_cloudwatch_event_rule.internal_entrypoint[each.key]
+    ]
+  }
 }
