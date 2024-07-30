a9b63b6df1004631765ed29c3a9b8b9cc4f078b4
Reuben Dunn <r.dunn@hypr.nz>
2024-07-31 10:22:18 +1200

    chore(example): updated default example

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -4,36 +4,46 @@ module "example_lambda_applcation" {
     aws = aws
   }
 
-  application_name    = "test_lambda_app"
+  application_name    = "service-env-example-com"
   application_runtime = "python3.8"
   application_version = "0.0.1"
-  artifact_bucket     = "labda-app.stage.example.com"
+  artifact_bucket     = "labda-app.env.example.com"
   artifact_bucket_key = "0.0.1/app.zip"
   application_memory  = 256
   application_timeout = 20
   layer_artifact_key  = "0.0.1/layers.zip"
 
+  parameter_store_path = "/service/env/environment/"
+
   lambda_functions_config = {
-    external_endpoint_function = {
-      name        = "external_endpoint_function"
-      description = "external_endpoint_function description"
+    ext-function = {
+      name        = "ext-function"
+      description = "external endpoint function description"
       handler     = "external_endpoint_function.handler.handle"
+      enable_vpc  = false
     },
-    internal_endpoint_function = {
-      name        = "internal_endpoint_function"
-      description = "internal_endpoint_function description"
+    int-function = {
+      name        = "int-function"
+      description = "internal endpoint function description"
       handler     = "internal_endpoint_function.handler.handle"
+      enable_vpc  = false
     }
   }
 
   internal_entrypoint_config = {
-    internal_endpoint_function = {
-      name               = "internal_endpoint_functionRule"
-      description        = "internal_endpoint_function Rule description"
+    int-function = {
+      name               = "int-function-rule"
+      description        = "internal endpoint function Rule description"
       event_pattern_json = { "source" : ["internal_endpoint_event.created"] }
     }
   }
 
+  enable_api_gateway       = true
+  api_gateway_route_config = {
+      ext-function = {
+      operation_name = "service:ext-function"
+    }
+  }
 
   application_env_vars = {
     name = "foo"
@@ -41,7 +51,7 @@ module "example_lambda_applcation" {
 
   enable_datastore                      = true
   create_dynamodb_table                 = true
-  dynamodb_table_name                   = "test_lambda_app"
+  dynamodb_table_name                   = "LA-service-env-example-com"
   dynamodb_hash_key                     = "PK"
   dynamodb_hash_key_type                = "S"
   dynamodb_range_key                    = "SK"
@@ -49,22 +59,10 @@ module "example_lambda_applcation" {
   dynamodb_autoscale_min_read_capacity  = 1
   dynamodb_autoscale_min_write_capacity = 1
 
-  dynamodb_global_secondary_index_map = [
-    {
-      name            = "Test-Index"
-      hash_key        = "HK"
-      range_key       = "RK"
-      write_capacity  = 1
-      read_capacity   = 1
-      projection_type = "KEYS_ONLY"
-    }
-  ]
-
   tags = {
     Environment = "stage"
     env         = "stage"
   }
-
 }
 
 provider "aws" {
